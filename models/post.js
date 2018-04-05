bcfc296c6c12b8b508a326bf6d02ea74b2168794
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2018-04-05 16:35:07 -0400

    added post scheme

---
+++ models/post.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var postSchema = mongoose.Schema({
+
+    title: String,
+    content: String,
+    comments: Array
+});
+// pushes in user:text pair when comment submits
+
+
+module.exports = mongoose.model('Post', postSchema);
